99d047604df51af0c5021238eba0d7735e6f2aae
migue1angel <macf06200@gmail.com>
2024-09-05 11:03:18 -0500

    create http server

--- 07-RESTweb/src/app.ts
+++ 07-RESTweb/src/app.ts
@@ -1,7 +1,25 @@
+import http from "http";
+import fs from "fs";
+const server = http.createServer((req, res) => {
+  console.log(req.url);
 
+  if (req.url === "/") {
+    const htmlFile = fs.readFileSync("src/public/index.html", "utf-8");
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end(htmlFile);
+    return;
+  }
 
-interface Person {
+  if (req.url?.endsWith(".js")) {
+    res.writeHead(200, { "Content-Type": "application/javascript" });
+  } else if (req.url?.endsWith(".css")) {
+    res.writeHead(200, { "Content-Type": "text/css" });
+  }
 
-}
+  const responseContent = fs.readFileSync(`src/public${req.url}`, "utf-8");
+  res.end(responseContent);
+});
 
-console.log('Hola mundo');
+server.listen(8080, () => {
+  console.log("Server listening on port 8080");
+});
