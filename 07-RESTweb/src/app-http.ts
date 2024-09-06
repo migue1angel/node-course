30807ea87fe46eba741342d97621d4572087308c
migue1angel <macf06200@gmail.com>
2024-09-06 09:28:00 -0500

    http2 protocol

--- 07-RESTweb/src/app-http.ts
+++ 07-RESTweb/src/app-http.ts
@@ -1,10 +1,6 @@
-import http2 from "http2";
+import http from "http";
 import fs from "fs";
-const server = http2.createSecureServer({
-  key:fs.readFileSync('keys/server.key'),
-  cert:fs.readFileSync('keys/server.crt')
-},
-(req, res) => {
+const server = http.createServer((req, res) => {
   console.log(req.url);
 
   if (req.url === "/") {
