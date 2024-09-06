5fad904d815f478dc44bd8194131bf42966c2587
migue1angel <macf06200@gmail.com>
2024-09-06 16:42:39 -0500

    env template and readme

---
+++ 07-RESTweb/README.md
@@ -0,0 +1,5 @@
+# Dev
+
+1. Clonar el .env.template
+2. Instalar módulos de node ""npm i""
+3. ejecutar npm run dev
--- 07-RESTweb/src/app.ts
+++ 07-RESTweb/src/app.ts
@@ -1,29 +1,14 @@
-import http2 from "http2";
-import fs from "fs";
-const server = http2.createSecureServer({
-  key:fs.readFileSync('keys/server.key'),
-  cert:fs.readFileSync('keys/server.crt')
-},
-(req, res) => {
-  console.log(req.url);
+import { envs } from "./config/envs";
+import { Server } from "./presentation/server";
 
-  if (req.url === "/") {
-    const htmlFile = fs.readFileSync("src/public/index.html", "utf-8");
-    res.writeHead(200, { "Content-Type": "text/html" });
-    res.end(htmlFile);
-    return;
-  }
+(() => {
+  main();
+})();
 
-  if (req.url?.endsWith(".js")) {
-    res.writeHead(200, { "Content-Type": "application/javascript" });
-  } else if (req.url?.endsWith(".css")) {
-    res.writeHead(200, { "Content-Type": "text/css" });
-  }
-
-  const responseContent = fs.readFileSync(`src/public${req.url}`, "utf-8");
-  res.end(responseContent);
-});
-
-server.listen(8080, () => {
-  console.log("Server listening on port 8080");
-});
+function main() {
+  const server = new Server({
+    port: envs.PORT,
+    public_path: envs.PUBLIC_PATH
+  });
+  server.start();
+}
