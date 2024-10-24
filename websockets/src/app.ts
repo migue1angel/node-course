78401be75eb306678e543eaa6e27f02e2abf426e
migue1angel <macf06200@gmail.com>
2024-10-24 14:05:25 -0500

    add websocket server

--- websockets/src/app.ts
+++ websockets/src/app.ts
@@ -1,1 +1,35 @@
-console.log('hello world');
+import { WebSocketServer, WebSocket } from "ws";
+
+const wss = new WebSocketServer({ port: 3000 });
+
+wss.on("connection", function connection(ws) {
+  console.log("Client connected");
+
+  ws.on("error", console.error);
+
+  ws.on("message", function message(data) {
+    const payload = JSON.stringify({
+      type: "custom-message",
+      payload: data.toString(),
+    });
+    //Send to everyone including itself
+    // wss.clients.forEach(function each(client) {
+    //   if (client.readyState === WebSocket.OPEN) {
+    //     client.send(payload, { binary: false });
+    //   }
+    // });
+    //Send to everyone excluding itself
+    wss.clients.forEach(function each(client) {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        client.send(payload, { binary: false });
+      }
+    });
+  });
+
+  // ws.send("Hello form server");
+
+  ws.on("close", () => {
+    console.log("Client disconnected");
+  });
+});
+console.log("Server running on port " + 3000);
